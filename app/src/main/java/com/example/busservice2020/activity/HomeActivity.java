65cb5322bc0eaa8cdcb93d8acf00dfdb7bee5934
Fuad <fuadhasanfhm@gmail.com>
2020-05-02 11:34:32 +0600

    To_Do_done

--- app/src/main/java/com/example/busservice2020/activity/HomeActivity.java
+++ app/src/main/java/com/example/busservice2020/activity/HomeActivity.java
@@ -10,11 +10,23 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.busservice2020.R;
+import com.example.busservice2020.model.UserModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 
 //ToDo Add user details to nav header
@@ -23,6 +35,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     private Toolbar toolbar;
     private DrawerLayout drawer;
     private NavigationView navigationView;
+    private ImageView imageView;
+    private TextView textView;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +49,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         toolbar = findViewById(R.id.home_toolbar);
         drawer = findViewById(R.id.home_drwerlayout);
         navigationView = findViewById(R.id.home_nav);
+        View hView = navigationView.inflateHeaderView(R.layout.nav_header);
+        textView = hView.findViewById(R.id.username_text);
+        imageView = hView.findViewById(R.id.user_image);
+
 
         setSupportActionBar(toolbar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
@@ -40,10 +60,39 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+
         if (savedInstanceState == null) {
-            navigationView.setCheckedItem(R.id.nav_item_about);
+            navigationView.setCheckedItem(R.id.admin);
 
         }
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        Log.d(TAG,""+firebaseUser.getUid());
+        final String userid = firebaseUser.getUid();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("userlist").child(userid);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                UserModel user = dataSnapshot.getValue(UserModel.class);
+                if (dataSnapshot.getValue() == null) {
+                    Log.d(TAG, "Why ? why ? ");
+                } else {
+                    Log.d(TAG, "not null"+user.getName());
+
+                    textView.setText(""+user.getName());
+                    Picasso.get().load(user.getImageURL()).into(imageView);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: database error" + databaseError.getMessage());
+            }
+        });
+
+
         navigationView.setNavigationItemSelectedListener(this);
 
     }
@@ -51,8 +100,17 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.nav_item_about:
-                Toast.makeText(this, "navigation about item slelected", Toast.LENGTH_SHORT).show();
+            case R.id.admin:
+                Toast.makeText(this, "navigation admin item slelected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this, "navigation  logout item slelected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ex_1:
+                Toast.makeText(this, "navigation Extra  item slelected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ex_2:
+                Toast.makeText(this, "navigation second Extra item slelected", Toast.LENGTH_SHORT).show();
                 break;
         }
 
